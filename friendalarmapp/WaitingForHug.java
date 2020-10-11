8ff087407d82cfe570e1bef3abdcf14b2661bd39
Camila Sinelli <71106968+camilasinelli@users.noreply.github.com>
2020-10-11 08:38:30 -0400

    Add files via upload

---
+++ friendalarmapp/WaitingForHug.java
@@ -0,0 +1,14 @@
+package com.example.friendalarmapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WaitingForHug extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiting_for_hug);
+    }
+}
